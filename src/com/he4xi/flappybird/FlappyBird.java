0596eda79c66bb430512e052505b32dc67b73823
Rando Rommot <rrommot@itcollege.ee>
2016-12-10 17:41:38 +0200

    Catalog tree initialized.

--- src/com/he4xi/flappybird/FlappyBird.java
+++ src/com/he4xi/flappybird/FlappyBird.java
@@ -1,8 +1,10 @@
+package com.he4xi.flappybird;
+
 /**
  * Blabalblabla
  * Created by rando on 10.12.16.
  */
-public class Test {
+public class FlappyBird {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
